06fe1385394f178015a3f65e9ddffd2a1b081365
chudee <geonwoo.chu@gmail.com>
2018-02-11 19:07:39 +0900

    comit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <p>Completed Tasks {{todos.filter(todo => todo.done === true).length }}</p>
+        <p>Pending Tasks {{todos.filter(todo => todo.done === false).length }}</p>
+        
+        <todo 
+            v-on:complete-todo="completeTodo" 
+            v-on:delete-todo="deleteTodo" 
+            v-for="(todo, index) in todos" 
+            v-bind:todo="todo" 
+            v-bind:key="index"
+        >
+        </todo>
+
+    </div>
+</template>
+
+<script type="text/javascript">
+    import Todo from './Todo'
+
+    export default {
+        props: ['todos'],
+        components: {
+            Todo
+        },
+        methods: {
+            deleteTodo(todo) {
+                const todoIndex = this.todos.indexOf(todo)
+                this.todos.splice(todoIndex, 1)
+            },
+            completeTodo(todo) {
+                const todoIndex = this.todos.indexOf(todo)
+                this.todos[todoIndex].done = true
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,3 @@
+Vue.component('ad', {
+    template: '<li>asdasd</li>'
+})
